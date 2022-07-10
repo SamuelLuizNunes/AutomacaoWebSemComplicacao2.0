efeeb451a6b6eddff7cfdd7dcf8a6bb575473a4c
Samuel Nunes <samuelluiznunes10@gmail.com>
2022-07-10 13:03:46 -0300

    adicionado test para o desafio de clicar no botão

--- src/test/java/br/com/chronosAcademy/automacaoWeb/TestWeb.java
+++ src/test/java/br/com/chronosAcademy/automacaoWeb/TestWeb.java
@@ -22,4 +22,35 @@ public class TestWeb {
         Assert.assertEquals("Porque Tempo É Conhecimento", txtTitulo.getText());
         driver.quit();
     }
+
+    @Test
+    public void deveClicarBotao(){
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://www.chronosacademy.com.br/");
+        String botao = "/html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[2]/div/div/a";
+        driver.findElement(By.xpath(botao)).click();
+        String txtConhecaCursos = "//*[@id=\"block-214\"]/div/div/div/div[1]/div/div";
+        Assert.assertEquals("Conheça todos os nossos cursos", driver.findElement(By.xpath(txtConhecaCursos)).getText());
+        driver.quit();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
