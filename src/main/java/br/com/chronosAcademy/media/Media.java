614ed5f9fef863759c428b525953f39fb2bfaaec
Samuel Nunes <samuelluiznunes10@gmail.com>
2022-07-09 23:10:16 -0300

    correção na classe de testes

--- src/main/java/br/com/chronosAcademy/media/Media.java
+++ src/main/java/br/com/chronosAcademy/media/Media.java
@@ -1,8 +1,6 @@
 package br.com.chronosAcademy.media;
 
 public class Media {
-
-    private double numero;
     public String calculaMedia(double valor1, double valor2){
         double media = (valor1 + valor2) / 2;
         if( media < 5){
