6bbd5abe7951244487d50bdca84f62106b2b194e
Samuel Nunes <samuelluiznunes10@gmail.com>
2022-07-12 23:26:36 -0300

    aula 06 anotações

--- src/test/java/br/com/chronosAcademy/automacaoWeb/TestWeb.java
+++ src/test/java/br/com/chronosAcademy/automacaoWeb/TestWeb.java
@@ -1,7 +1,9 @@
 package br.com.chronosAcademy.automacaoWeb;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -9,32 +11,31 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestWeb {
 
-    @Test
-    public void primeiroTeste(){
+    private  ChromeDriver driver;
+    @Before
+    public void inicializaDriver(){
         WebDriverManager.chromedriver().setup();
-        ChromeDriver driver = new ChromeDriver();
-
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.chronosacademy.com.br");
-
+    }
+    @After
+    public void finalizaDriver(){
+        driver.quit();
+    }
+    @Test
+    public void primeiroTeste(){
         String xPathTitulo = "/html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[1]/div/h4";
         WebElement txtTitulo =  driver.findElement(By.xpath(xPathTitulo));
         Assert.assertEquals("Porque Tempo É Conhecimento", txtTitulo.getText());
-        driver.quit();
     }
 
     @Test
-    public void deveClicarBotao(){
-        WebDriverManager.chromedriver().setup();
-        ChromeDriver driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
-        driver.get("https://www.chronosacademy.com.br/");
+    public void deveClicarLink(){
         String botao = "/html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[2]/div/div/a";
         driver.findElement(By.xpath(botao)).click();
         String txtConhecaCursos = "//*[@id=\"block-214\"]/div/div/div/div[1]/div/div";
         Assert.assertEquals("Conheça todos os nossos cursos", driver.findElement(By.xpath(txtConhecaCursos)).getText());
-        driver.quit();
     }
 
 
