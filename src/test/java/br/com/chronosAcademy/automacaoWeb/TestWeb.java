fbc7328f4482a1c0c2bdf3c842dcea6cf7434e81
Samuel Nunes <samuelluiznunes10@gmail.com>
2022-07-14 01:15:24 -0300

    Aplicando Page Object

--- src/test/java/br/com/chronosAcademy/automacaoWeb/TestWeb.java
+++ src/test/java/br/com/chronosAcademy/automacaoWeb/TestWeb.java
@@ -1,6 +1,8 @@
 package br.com.chronosAcademy.automacaoWeb;
 
 import br.com.chronosAcademy.core.Driver;
+import br.com.chronosAcademy.pages.CursoPage;
+import br.com.chronosAcademy.pages.PrincipalPage;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -9,34 +11,36 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+
 public class TestWeb {
-    WebDriver driver;
     Driver driverWeb;
+    PrincipalPage pricipalPage;
+    CursoPage cursoPage;
 
     @Before
     public void inicializaDriver(){
         driverWeb = new Driver("chrome");
-        driver = driverWeb.getDriver();
-        driver.get("https://www.chronosacademy.com.br");
+        driverWeb.getDriver().get("https://www.chronosacademy.com.br");
+        pricipalPage = new PrincipalPage(driverWeb.getDriver());
+        cursoPage = new CursoPage(driverWeb.getDriver());
     }
     @After
     public void finalizaDriver(){
-        driver.quit();
-    }
-    @Test
-    public void primeiroTeste(){
-        String xPathTitulo = "//section[2]//h4";
-        WebElement txtTitulo =  driver.findElement(By.xpath(xPathTitulo));
-        Assert.assertEquals("Porque Tempo É Conhecimento", txtTitulo.getText());
+        driverWeb.getDriver().quit();
     }
 
     @Test
-    public void deveClicarLink(){
-        String botao = "//section[2]//div[3]//a";
-        driver.findElement(By.xpath(botao)).click();
-        String txtConhecaCursos = "//*[@id=\"block-214\"]/div/div/div/div[1]/div/div";
-        Assert.assertEquals("Conheça todos os nossos cursos",
-                driver.findElement(By.xpath(txtConhecaCursos)).getText());
+    public void primeiroTeste(){
+        Assert.assertEquals("Porque Tempo É Conhecimento", pricipalPage.getTitulo());
     }
+
+    @Test
+    public void segundoTeste(){
+        pricipalPage.clickBotao();
+        Assert.assertEquals("Conheça todos os nossos cursos", cursoPage.getTitulo2());
+    }
+
+
+
 
 }
